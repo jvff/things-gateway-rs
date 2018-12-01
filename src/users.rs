834b6b911468d45126950281f6b916791dc9ff00
Janito Vaqueiro Ferreira Filho <janito@mullvad.net>
2018-12-01 02:15:56 -0200

    Run rustfmt on users module

--- src/users.rs
+++ src/users.rs
@@ -1,15 +1,17 @@
 use std::collections::BTreeMap;
 use std::sync::{Arc, RwLock};
 
-type User = (String, String); 
+type User = (String, String);
 
 pub struct UserService {
-    db: Arc<RwLock<BTreeMap<usize, User>>>
+    db: Arc<RwLock<BTreeMap<usize, User>>>,
 }
 
 impl UserService {
     pub fn new() -> UserService {
-        UserService { db: Arc::new(RwLock::new(BTreeMap::default())) }
+        UserService {
+            db: Arc::new(RwLock::new(BTreeMap::default())),
+        }
     }
 
     pub fn with_db(db: Arc<RwLock<BTreeMap<usize, User>>>) -> UserService {
@@ -21,9 +23,8 @@ impl UserService {
     }
 
     pub fn add(&mut self, user: User) -> usize {
-        let user_id: usize =  { 
-            
-            // This is terrible. 
+        let user_id: usize = {
+            // This is terrible.
             let mut id: usize = rand::random::<usize>();
             while self.db.read().unwrap().contains_key(&id) {
                 id = rand::random::<usize>();
@@ -31,7 +32,6 @@ impl UserService {
             id
         };
 
-            
         self.db.write().unwrap().insert(user_id, user);
         user_id
     }
